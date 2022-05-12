c38107585808a12515be3fee6cae26173bd3c942
UsefSaeed <nyngob-jYnbab-8tichy>
2022-05-12 19:16:11 +0200

    Batman Begins

---
+++ src/PerfectHashing/Method1.java
@@ -0,0 +1,16 @@
+package PerfectHashing;
+
+public class Method1 {
+    private int[] input;
+    private int size;
+
+    public Method1(int[] input) {
+        this.input = input;
+        this.size = input.length*input.length;
+    }
+
+    int[] generateHashMatrix(){
+        int[] res = new int[size];
+        return  res;
+    }
+}
---
+++ src/PerfectHashing/Method2.java
@@ -0,0 +1,16 @@
+package PerfectHashing;
+
+public class Method2 {
+    private int[] input;
+    private int size;
+
+    public Method2(int[] input) {
+        this.input = input;
+        this.size = input.length;
+    }
+
+    int[] generateHashMatrix(){
+        int[] res = new int[size];
+        return res;
+    }
+}
